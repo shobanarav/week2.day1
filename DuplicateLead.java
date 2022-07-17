a0e570273e5efa3d920910f5dbd86ca954de134c
shobanarav <108995172+shobanarav@users.noreply.github.com>
2022-07-17 13:24:20 +0530

    Add files via upload

---
+++ DuplicateLead.java
@@ -0,0 +1,70 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Test");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Leaf");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Information Technology");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("this is just for practice");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("shobanasin.92@gmail.com");
+		
+		WebElement state = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		
+		Select stateList = new Select(state);
+		stateList.selectByVisibleText("New York");
+		
+		driver.findElement(By.className("smallSubmit")).click();
+		
+		String title = driver.getTitle();
+		if(title.equals("View Lead | opentaps CRM"))
+				{
+		System.out.println(title);
+				}
+		
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		
+		WebElement compName = driver.findElement(By.id("createLeadForm_companyName"));
+		compName.clear();
+		
+		WebElement firstName = driver.findElement(By.id("createLeadForm_firstName"));
+		firstName.clear();
+		
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("NewLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("New");
+		
+        driver.findElement(By.className("smallSubmit")).click();
+		
+        String title2 = driver.getTitle();
+		if(title2.equals("View Lead | opentaps CRM"))
+				{
+		System.out.println("Title 2 :" +title2);
+				}
+
+	}
+
+}
